5abd4529e14de9c0374e3832e9eab3f86818e5f0
Jonathan Pearl <Jonathan Pearl>
2017-04-08 14:33:24 -0700

    Initial project creation

---
+++ Source/Sadsads/SadsadsGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "SadsadsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SADSADS_API ASadsadsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
